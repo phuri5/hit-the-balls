9a72dcb660c75cf294f616c9afe472b64fa4b451
phuri5 <140503836+phuri5@users.noreply.github.com>
2023-07-25 17:20:55 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (iBIT.ReadADC(ibitReadADC.ADC1)))
 })
-input.onButtonPressed(Button.AB, function () {
-	
-})
 input.onButtonPressed(Button.B, function () {
     basic.showString("" + (iBIT.ReadADC(ibitReadADC.ADC2)))
 })
